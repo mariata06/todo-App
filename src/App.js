19b17aad4a7da2f9ea607711b0aba76dd520d6f2
mariata06 <slinkywizard@mail.ru>
2023-01-17 17:41:05 +0300

    Подготовка заготовок для компонентов: todo задачи, списка, формы ввода

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import TodoList from './components/Todos/TodoList';
+import TodoForm from './components/Todos/TodoForm';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <h1>My Todo App on React</h1>
+      <TodoForm />
+      <TodoList />
+    </div>
   );
 }
 
